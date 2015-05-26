ac1e25d1d4d7d220802ccfac69d26429451ebc6a
hrk fkm <fkm@fkmsoft.jp>
2015-05-26 18:35:31 +0900

    Add response struct

--- gcm.go
+++ gcm.go
@@ -4,3 +4,6 @@ type GCMClient interface {
 	// Sends GCM message to GCM server.
 	Send(apiKey string, data map[string]interface{}, regIds ...string) (Response, error)
 }
+
+type Response struct {
+}
