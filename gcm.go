373505023fd3aaed8ade4018c5dabb436c02305d
hrk fkm <fkm@fkmsoft.jp>
2015-05-26 18:33:22 +0900

    Updates signature

--- gcm.go
+++ gcm.go
@@ -2,5 +2,5 @@ package gcm
 
 type GCMClient interface {
 	// Sends GCM message to GCM server.
-	Send(apiKey string, data map[string]interface{}, regIds ...string)
+	Send(apiKey string, data map[string]interface{}, regIds ...string) (Response, error)
 }
